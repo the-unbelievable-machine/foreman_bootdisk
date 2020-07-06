bca0f37b87ce35e245f2775a7cf82a4c7d66d887
andreas.klamke <andreas.klamke@unbelievable-machine.com>
2020-07-06 18:41:42 +0200

    try to use another ssh port

--- app/models/concerns/foreman_bootdisk/compute_resources/proxmox.rb
+++ app/models/concerns/foreman_bootdisk/compute_resources/proxmox.rb
@@ -21,7 +21,7 @@ module ForemanBootdisk
           'boot' => 'dcn'
         }
         server.update(config_attributes)
-        server.ssh_options = { user: 'ansible', auth_methods: ['publickey'], keys: ['/home/foreman/.ssh/id_rsa'], port: '22001' }
+        server.ssh_options = { user: 'ansible', auth_methods: ['publickey'], keys: ['/home/foreman/.ssh/id_rsa'], port: 22001 }
         #server.ssh_ip_address = bridges.first.address
         server.ssh_ip_address = interfaces.first.address
         server.scp_upload(iso, '/var/lib/vz/template/iso/')
